39387ec7a3fbe29826c8e52f2c839cd5ea966948
astrojet <53126546+astrojet@users.noreply.github.com>
2020-08-02 00:43:18 -0700

    Update getworkers.go

--- libbeat/outputs/lumberjack/getworkers.go
+++ libbeat/outputs/lumberjack/getworkers.go
@@ -2,58 +2,105 @@ package lumberjack
 
 import (
 	"bytes"
+	"container/ring"
 	b64 "encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/elastic/beats/libbeat/outputs/lumberjack/protobufs"
-	"github.com/golang/protobuf/proto"
 	"io/ioutil"
 	"net/http"
 	"time"
-	"errors"
+	"strings"
+	"github.com/elastic/beats/libbeat/outputs/lumberjack/protobufs"
+	"github.com/golang/protobuf/proto"
 )
 
 var (
-	MasterNode string
-	MasterPort string
-	workerNodes = make(map[string][]Node, 0)
-	currentWorkerIndexes = make(map[string]int, 0)
-	markdownNamespaces = make([]string, 0)
+	MasterNode              string
+	MasterPort              string
+	workerNodeRingsAllNamespaces         = make(map[string]*ring.Ring)
+	markdownNamespaces      = make([]string, 0)
+	masterConnectFailures   = 0
+	masterConnectLastFailed = time.Now()
 )
 
-func GetWorkersWithRetries(namespace map[string]string) ([]Node, error){
+func GetWorkersWithRetries(namespace map[string]string) (*ring.Ring, error) {
 	namespaceKey := GetDelimitedNamespaceKey(namespace)
-	if nodes, ok :=  workerNodes[namespaceKey] ; ok {
-		return nodes,nil
+	if nodes, ok := workerNodeRingsAllNamespaces[namespaceKey]; ok {
+		return nodes, nil
 	}
 
-	workers, err := GetWorkers(namespace)
-	if err != nil {
-		time.Sleep(2 * time.Second)
-		workers, err = GetWorkers(namespace)
+	if masterConnectFailures < 3 {
+		workers, err := GetWorkers(namespace)
+		if err != nil {
+			time.Sleep(100 * time.Millisecond)
+			workers, err = GetWorkers(namespace)
+		}
+		return workers, err
+	} else {
+		elapsedMins := time.Since(masterConnectLastFailed).Minutes()
+		if elapsedMins > 1 {
+			masterConnectFailures = 0
+			workers, err := GetWorkers(namespace)
+			if err != nil {
+				time.Sleep(100 * time.Millisecond)
+				workers, err = GetWorkers(namespace)
+			}
+			return workers, err
+		} else {
+			msg := "Master node looks to be down...last retry less than 5 mins ago...standing down"
+			return nil, errors.New(msg)
+		}
 	}
-	return workers,err
 }
 
-func GetWorkers(namespace map[string]string) ([]Node, error){
+func GetWorkers(namespace map[string]string) (*ring.Ring, error) {
 	namespaceKey := GetDelimitedNamespaceKey(namespace)
 	namespaceWorkers, getWorkersError := GetWorkersForNamespace(LumberConfig, namespace)
 	if getWorkersError != nil {
 		Error.Println("Error occurred when getting workers from Lumberjack for namespace ["+namespaceKey+"]:", getWorkersError)
-		return nil,getWorkersError
+		return nil, getWorkersError
 	} else {
 		if len(namespaceWorkers) > 0 {
-			workerNodes[namespaceKey] = namespaceWorkers
+			namespaceWorkers = getReplicaNodes(namespaceWorkers)
+			r := ring.New(len(namespaceWorkers))
+			for _, workerNode := range namespaceWorkers {
+				r.Value = workerNode
+				r = r.Next()
+			}
+			workerNodeRingsAllNamespaces[namespaceKey] = r
 			Info.Println("----- Lumberjack workers for namespace [" + namespaceKey + "] ----")
-			Info.Println(namespaceWorkers)
+			for _, workerNode := range namespaceWorkers {
+				Info.Println(workerNode.IpPort)
+			}
 			Info.Println("--------------------------------------------------------------")
-			return namespaceWorkers,nil
+			return r, nil
 		} else {
 			msg := "Error occurred when getting workers from Lumberjack for namespace [" + namespaceKey + "]: Got Empty Worker List!"
 			Error.Println(msg)
-			return nil,errors.New(msg)
+			return nil, errors.New(msg)
 		}
 	}
+}
+
+func getReplicaNodes(workerNodes []Node) []Node {
+	workerNodeMap := make(map[int]Node, 0)
+	for _, workerNode := range workerNodes {
+		workerNodeMap[workerNode.Id] = workerNode
+	}
+	workerNodesWithReplicas := make([]Node, 0)
+
+	for _, workerNode := range workerNodes {
+		workerReplicas := make([]Node, 0)
+		replicaIds := workerNode.ReplicaIds
+		for _, replicaId := range replicaIds {
+			replicaNode := workerNodeMap[replicaId]
+			workerReplicas = append(workerReplicas, replicaNode)
+		}
+		workerNode.Replicas = workerReplicas
+		workerNodesWithReplicas = append(workerNodesWithReplicas, workerNode)
+	}
+	return workerNodesWithReplicas
 }
 
 func GetWorkersForNamespace(c lumberjackConfig, namespace map[string]string) ([]Node, error) {
@@ -74,22 +121,27 @@ func GetWorkersForNamespace(c lumberjackConfig, namespace map[string]string) ([]
 	inputArr := b64.URLEncoding.EncodeToString(data)
 	req, _ := http.NewRequest("POST", getWorkersAPI, bytes.NewBuffer([]byte(inputArr)))
 	req.Header.Add("Content-Type", "application/protobuf")
-	res, connectErr := http.DefaultClient.Do(req)
+	client := &http.Client{Timeout: 200 * time.Millisecond}
+	res, connectErr := client.Do(req)
+
 	if connectErr != nil {
+		masterConnectFailures = masterConnectFailures + 1
+		masterConnectLastFailed = time.Now()
 		//Error.Println(errors.New("Unable to get worker nodes form lumberjack. May be the cluster is not up and running...Skipping for now: "+connectErr.Error()));
 		return nil, connectErr
 	}
+	masterConnectFailures = 0
 	defer res.Body.Close()
 	//Debug.Println("get workers response: ", res)
 	resJson, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil{
+	if readErr != nil {
 		//Error.Println("Invalid response form lumberjack when getting worker nodes.: "+readErr.Error());
 		return nil, readErr
 	}
 	//Debug.Println("get workers response json: ", resJson)
 	getworkersResponse, decodeErr := b64.URLEncoding.DecodeString(string(resJson))
 
-	if decodeErr != nil{
+	if decodeErr != nil {
 		//Error.Println(errors.New("Error occurred when decoding get workers response from lumberjack.: "+decodeErr.Error()));
 		return nil, decodeErr
 	}
@@ -106,9 +158,32 @@ func GetWorkersForNamespace(c lumberjackConfig, namespace map[string]string) ([]
 	return workers, nil
 }
 
-func GetDelimitedNamespaceKey(namespace map[string]string) string{
-	namespaceKey := namespace["a_dc"]+"_"+namespace["b_app"]+"_"+namespace["c_type"]+"_"+namespace["d_feature"]+"_"+namespace["e_instance"];
-	return namespaceKey;
+func GetDelimitedNamespaceKey(namespace map[string]string) string {
+	namespaceKey := namespace["a_dc"] + "_" + namespace["b_app"] + "_" + namespace["c_type"] + "_" + namespace["d_feature"] + "_" + namespace["e_instance"]
+	return namespaceKey
+}
+
+func getNamespaceMapFromKey(namespaceKey string) map[string]string{
+	namespaceParts := strings.Split(namespaceKey, "_")
+
+	namespace := make(map[string]string, 0)
+	namespace["a_dc"] = namespaceParts[0]
+	namespace["b_app"] = namespaceParts[1]
+	namespace["c_type"] = namespaceParts[2]
+	namespace["d_feature"] = namespaceParts[3]
+	namespace["e_instance"] = namespaceParts[4]
+	return namespace
+}
+
+
+func getNamespaceMap(logRecordNamespace LumberjackNameSpace) map[string]string{
+	namespace := make(map[string]string, 0)
+	namespace["a_dc"] = logRecordNamespace.A_dc
+	namespace["b_app"] = logRecordNamespace.B_app
+	namespace["c_type"] = logRecordNamespace.C_type
+	namespace["d_feature"] = logRecordNamespace.D_feature
+	namespace["e_instance"] = logRecordNamespace.E_instance
+	return namespace
 }
 
 type Node struct {
@@ -117,11 +192,12 @@ type Node struct {
 	IpPort       IpPort `json:"ipPort"`
 	PublicIpPort IpPort `json:"publicIpPort"`
 	ParentIds    []int  `json:"parentIds"`
-	ReplicaIds   [] int `json:"replicaIds"`
+	ReplicaIds   []int  `json:"replicaIds"`
 	ReadOnly     bool   `json:"readOnly"`
+	Replicas     []Node
 }
 
 type IpPort struct {
 	Ip   string `json:"ip"`
-	Port int `json:"port"`
+	Port int    `json:"port"`
 }
