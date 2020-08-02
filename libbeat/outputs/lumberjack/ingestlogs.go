76d8d2802ee7199b4b3cf26548d8b0fe265f5465
astrojet <53126546+astrojet@users.noreply.github.com>
2020-08-02 00:43:38 -0700

    Update ingestlogs.go

--- libbeat/outputs/lumberjack/ingestlogs.go
+++ libbeat/outputs/lumberjack/ingestlogs.go
@@ -2,100 +2,155 @@ package lumberjack
 
 import (
 	"bytes"
+	"container/ring"
 	b64 "encoding/base64"
+	"errors"
 	"fmt"
-	"github.com/elastic/beats/libbeat/common"
-	"github.com/elastic/beats/libbeat/outputs/lumberjack/protobufs"
-	"github.com/elastic/beats/libbeat/publisher"
-	"github.com/golang/protobuf/proto"
 	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
-	"errors"
+
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/outputs/lumberjack/protobufs"
+	"github.com/elastic/beats/libbeat/publisher"
+	"github.com/golang/protobuf/proto"
 )
 
-var(
+var (
 	ingestLogsParallelism = 10
+	apps                  = make(map[string]string)
+	instances             = make(map[string]string)
+	namespaceLogSizes     = make(map[string]int)
 )
 
-func IngestLogs(batch publisher.Batch) int {
-	dropped := 0
+func PrintMetrics() {
+	metricsTicker := time.NewTicker(60 * time.Second)
+	for _ = range metricsTicker.C {
+		Info.Println("--------------------- namespaces log Sizes -------------------------")
+		for namespace, logSize := range namespaceLogSizes {
+			fmt.Println("namespace:", namespace, "=>", "log Size KB:", logSize/1000)
+			namespaceLogSizes[namespace] = 0
+		}
+	}
+}
+
+func IngestLogs(batch publisher.Batch)  {
 	events := batch.Events()
+	namespaceLogRecords := make(map[string] []*LumberjackLogRecord)
 	for i := range events {
 		event := &events[i]
-		if event != nil && LumberConfig.PostMetrics == true{
-			lumberjackLogRecord, logRecordErr := getLogRecord(event)
+		if event != nil {
+			logRecord, logRecordErr := getLogRecord(event)
 			if logRecordErr != nil {
 				Error.Println("Error occurred when creating Lumberjack Log Record from Beats Event", logRecordErr)
 			} else {
-				ingestLogRecord(&lumberjackLogRecord)
+				logRecordNamespace := logRecord.Namespace
+				apps[logRecordNamespace.B_app] = logRecordNamespace.B_app
+				instances[logRecordNamespace.E_instance] = logRecordNamespace.E_instance
+
+				namespace := getNamespaceMap(logRecordNamespace)
+				namespaceKey := GetDelimitedNamespaceKey(namespace)
+				logRecords, found := namespaceLogRecords[namespaceKey];
+				if !found {
+					logRecords = make([]*LumberjackLogRecord, 0)
+				}
+				logRecords = append(logRecords, &logRecord)
+				namespaceLogRecords[namespaceKey] = logRecords
 			}
 		}
 	}
-	return dropped
+	ingestLogRecords(namespaceLogRecords)
 }
 
-func ingestLogRecord(logRecord *LumberjackLogRecord) error {
-	logRecordNamespace := logRecord.Namespace
+func ingestLogRecords(namespaceLogRecords map[string] []*LumberjackLogRecord) error {
 
-	namespace := make(map[string]string, 0)
-	namespace["a_dc"] = logRecordNamespace.A_dc
-	namespace["b_app"] = logRecordNamespace.B_app
-	namespace["c_type"] = logRecordNamespace.C_type
-	namespace["d_feature"] = logRecordNamespace.D_feature
-	namespace["e_instance"] = logRecordNamespace.E_instance
+	for namespaceKey, logRecords := range namespaceLogRecords {
+		namespace := getNamespaceMapFromKey(namespaceKey)
+		workerNodes, getWorkersErr := GetWorkersWithRetries(namespace)
 
-	workerNodes, getWorkersErr := GetWorkersWithRetries(namespace)
-
-	if len(workerNodes) > 0 && getWorkersErr == nil{
-		namespaceKey := GetDelimitedNamespaceKey(namespace)
-		currentWorkerIndex := currentWorkerIndexes[namespaceKey]
-
-		if currentWorkerIndex >= len(workerNodes) {
-			currentWorkerIndex = 0
+		if workerNodes.Len() > 0 && getWorkersErr == nil {
+			//TODO check if dual write is enabled through a setting
+			//dualWriteEnabled := true
+			error := sendIngestLogsToWorker(workerNodes, logRecords, namespace, namespaceKey, "primary")
+			if error != nil {
+				return error
+			}
+			/*if dualWriteEnabled {
+				replicaNodes := workerNode.Replicas
+				for _, replicaNode := range replicaNodes {
+					sendIngestLogsToWorker(replicaNodes, logRecord, namespace, namespaceKey, "replica")
+				}
+			}*/
+		} else {
+			return errors.New("No workers available to ingest the log record")
 		}
-		currentWorkerIndexes[namespaceKey] = currentWorkerIndex
-		workerNode := workerNodes[currentWorkerIndex]
-		workerIpPort := workerNode.IpPort
+	}
 
-		ingestLogsAPI := fmt.Sprintf("http://%s:%d/", workerIpPort.Ip, workerIpPort.Port)
-		//ingestLogsAPI := fmt.Sprintf("http://%s:%d/", "host.docker.internal", workerIpPort.Port)
 
-		cmdType := protobufs.CommandType_WRITE_LOG
-		metaData := protobufs.Metadata{Namespace: namespace}
+	return nil
+}
+
+func sendIngestLogsToWorker(workerNodes *ring.Ring, logRecords []*LumberjackLogRecord, namespace map[string]string, namespaceKey string, nodeType string) error {
+	workerNode := workerNodes.Value.(Node)
+	workerIpPort := workerNode.IpPort
+	ingestLogsAPI := fmt.Sprintf("http://%s:%d/", workerIpPort.Ip, workerIpPort.Port)
+	//ingestLogsAPI := fmt.Sprintf("http://%s:%d/", "host.docker.internal", workerIpPort.Port)
+
+	cmdType := protobufs.CommandType_WRITE_LOG
+	metaData := protobufs.Metadata{Namespace: namespace}
+
+	logLines := make([]*protobufs.LogLine, 0)
+	for _,logRecord := range logRecords {
 		timestamp := logRecord.Timestamp.UnixNano() / 1000000
 		content := logRecord.Message
+
+		logMsgBytes := len(logRecord.Message)
+		if logSize, ok := namespaceLogSizes[namespaceKey]; ok {
+			namespaceLogSizes[namespaceKey] = logSize+logMsgBytes
+		}else{
+			namespaceLogSizes[namespaceKey] = logMsgBytes
+		}
+
 		line := protobufs.LogLine{TimeStamp: &timestamp, Content: &content}
-		loglines := make([]*protobufs.LogLine, 0)
-		loglines = append(loglines, &line)
-		commsEnvelop := protobufs.CommsEnvelop{
-			CommandType: &cmdType,
-			LogLineSet:  &protobufs.LogLineSet{LogLines: loglines, Metadata: &metaData},
-		}
-
-		data, err := proto.Marshal(&commsEnvelop)
-		if err != nil {
-			return err
-		}
-
-		postErr := postLogRecord(ingestLogsAPI, data, workerNode, namespaceKey, currentWorkerIndex)
-
-		if postErr != nil {
-			time.Sleep(2 * time.Second)
-			postErr = postLogRecord(ingestLogsAPI, data, workerNode, namespaceKey, currentWorkerIndex)
-		}
-		return postErr
-	}else{
-		return errors.New("No workers available to ingest the log record")
+		logLines = append(logLines, &line)
 	}
+	commsEnvelop := protobufs.CommsEnvelop{
+		CommandType: &cmdType,
+		LogLineSet:  &protobufs.LogLineSet{LogLines: logLines, Metadata: &metaData},
+	}
+
+	data, err := proto.Marshal(&commsEnvelop)
+	if err != nil {
+		return err
+	}
+
+	postErr := postLogRecord(ingestLogsAPI, data, len(logLines), workerNodes, namespaceKey)
+	if postErr != nil {
+		Error.Println("Error occurred when posting log record ", postErr)
+	}
+
+	if postErr != nil {
+		time.Sleep(500 * time.Millisecond)
+		postErr = postLogRecord(ingestLogsAPI, data, len(logLines), workerNodes, namespaceKey)
+		if postErr != nil {
+			time.Sleep(500 * time.Millisecond)
+			postErr = postLogRecord(ingestLogsAPI, data, len(logLines), workerNodes, namespaceKey)
+			workerNodes = moveToNextWorker(namespaceKey, workerNodes)
+			nextWorker := workerNodes.Value.(Node)
+			Error.Println("Worker Down!!...Switching from [", workerIpPort, "]to next worker[", nextWorker.IpPort, "] for namespace ["+namespaceKey+"] ")
+		}
+	}
+	return nil
 }
 
-func postLogRecord(ingestLogsAPI string, data []byte, workerNode Node, namespaceKey string, currentWorkerIndex int) error{
+func postLogRecord(ingestLogsAPI string, data []byte, noOfLogLines int, workerNodes *ring.Ring, namespaceKey string) error {
+	workerNode := workerNodes.Value.(Node)
 	inputArr := b64.URLEncoding.EncodeToString(data)
 	req, _ := http.NewRequest("POST", ingestLogsAPI, bytes.NewBuffer([]byte(inputArr)))
 	req.Header.Add("Content-Type", "application/protobuf")
-	res, httpErr := http.DefaultClient.Do(req)
+	client := &http.Client{Timeout: 500 * time.Millisecond}
+	res, httpErr := client.Do(req)
 
 	if httpErr != nil {
 		Error.Println("Error connecting to lumberjack worker [", workerNode, "] for namespace [", namespaceKey, "]: ", httpErr)
@@ -119,10 +174,12 @@ func postLogRecord(ingestLogsAPI string, data []byte, workerNode Node, namespace
 				executionStatus := respEnvelop.ExecutionStatus
 				if responseCmdType != nil && executionStatus != nil {
 					if (*responseCmdType == protobufs.CommandType_ACK) && *executionStatus {
-
+						Debug.Println("Sent [",noOfLogLines,"] log lines for  namespace [",namespaceKey,"] to worker [",workerNode.IpPort,"]")
 					} else if *responseCmdType == protobufs.CommandType_BLOCK_FULL {
-						currentWorkerIndexes[namespaceKey] = currentWorkerIndex+1
-						Error.Println("Worker Block Full!!...Switching to next worker for namespace ["+namespaceKey+"] ")
+						workerNodes = moveToNextWorker(namespaceKey, workerNodes)
+						nextWorker := workerNodes.Value.(Node)
+						Error.Println("Worker Block Full!!...Switching from [", workerNode.IpPort, "] to next worker [", nextWorker.IpPort, "] for namespace ["+namespaceKey+"] ")
+						workerNode = nextWorker
 					} else {
 						Error.Println("Unknown Ingest Logs Response. Got Response Command: ", responseCmdType, " for namespace ["+namespaceKey+"]")
 					}
@@ -136,23 +193,23 @@ func postLogRecord(ingestLogsAPI string, data []byte, workerNode Node, namespace
 			}
 		}
 	}
+	return nil
+}
+
+func moveToNextWorker(namespaceStr string, workerNodes *ring.Ring) *ring.Ring {
+	workerNodes = workerNodes.Next()
+	workerNodeRingsAllNamespaces[namespaceStr] = workerNodes
+	return   workerNodes
 }
 
 func getLogRecord(event *publisher.Event) (LumberjackLogRecord, error) {
 	var lumberjackRecord LumberjackLogRecord
 	content := event.Content
 	time := content.Timestamp
-
-	//Debug.Println("getLogRecord: time: ", time)
-	message, messageErr := content.Fields.GetValue("message")
-	if messageErr != nil {
-		//Error.Println("Error getting message from log: ", messageErr)
-	}
+	message, _ := content.Fields.GetValue("message")
 
 	messageStr := fmt.Sprintf("%v", message)
 	log, logErr := content.Fields.GetValue("log")
-	//Debug.Println("getLogRecord: log: ", log)
-	//Debug.Println("getLogRecord: logErr: ", logErr)
 
 	var namespace LumberjackNameSpace
 	if logErr == nil && log != "" {
@@ -180,9 +237,6 @@ func getLogRecord(event *publisher.Event) (LumberjackLogRecord, error) {
 		//Error.Println("Log Event doesnt contain log file name and path info. This is needed for namespace info to push logs!")
 		return lumberjackRecord, logErr
 	}
-	//Debug.Println("getLogRecord: LumberjackLogRecord: time: ", time)
-	//Debug.Println("getLogRecord: LumberjackLogRecord: messageStr: ", messageStr)
-	//Debug.Println("getLogRecord: LumberjackLogRecord: namespace: ", namespace)
 	lumberjackRecord = LumberjackLogRecord{time, messageStr, namespace}
 	return lumberjackRecord, nil
 }
